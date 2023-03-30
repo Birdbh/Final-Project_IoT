9b49963c72d134a0576778f55224d77661e18319
Birdbh <Birdlkjlkj123@gmail.com>
2023-03-30 09:14:53 +0200

    removed useless comments and changed write fields

--- Light_ESP32_Thinkspeak.ino
+++ Light_ESP32_Thinkspeak.ino
@@ -8,16 +8,6 @@ const char* password = "test123456";
 
 WiFiClient client;
 
-const int DHTPin = D3;
-// Initialize DHT sensor.
-DHT dht(DHTPin, DHTTYPE);
-
-// Temporary variables
-/*static char celsiusTemp[7];
-static char fahrenheitTemp[7];
-static char humidityTemp[7];
-*/
-
 unsigned long channelID = 2077705; //your channel
 const char * myWriteAPIKey = "EK8E6JY8O4DX0T6X"; // your WRITE API key
 const char* server = "api.thingspeak.com";
@@ -29,8 +19,6 @@ void setup() {
   // Initializing serial port for debugging purposes
   Serial.begin(115200);
   delay(10);
-
-  dht.begin();
   
   // Connecting to WiFi network
   Serial.println();
@@ -59,26 +47,15 @@ void loop() {
     Serial.print("RSSI: ");
     Serial.println(rssi); 
 
-
-    ThingSpeak.setField(4,rssi);
-
-// **** This part reads only sensors and calculates
-            // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-            /*float h = dht.readHumidity();
-            // Read temperature as Celsius (the default)
-            float t = dht.readTemperature();
-            // Read temperature as Fahrenheit (isFahrenheit = true)
-            float f = dht.readTemperature(true);*/
-            // Check if any reads failed and exit early (to try again).
-            int lightValue = analogRead(A0);
-            if (isnan(lightValue)) {
-              Serial.println("Failed to read from light sensor!");    
-            }
-            else{
-              Serial.print(lightValue);
-        //end of sensor readings
-              ThingSpeak.setField(2,lightValue);
-            }
+    int lightValue = analogRead(A0);
+    if (isnan(lightValue)) {
+      Serial.println("Failed to read from light sensor!");    
+    }
+    else{
+      Serial.print(lightValue);
+      //end of sensor readings
+      ThingSpeak.setField(2,lightValue);
+    }
             
       ThingSpeak.writeFields(channelID, myWriteAPIKey);
   }
